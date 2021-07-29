845fd94a79a3d99392f946168ef1360cd74ccaf9
Berno2143 <atomasbernardino@gmail.com>
2021-07-28 19:21:51 -0500

    Text

--- ver1.2_ProyectAshba/listas.js
+++ ver1.2_ProyectAshba/listas.js
@@ -1,50 +1,11 @@
-var checkbox1 = document.getElementById('checkbox1');
-checkbox1.addEventListener( 'change', function() {
-    if(this.checked) {
-        document.getElementById('total').innerHTML = "$1,999.00";
-    } 
-    else{
-        document.getElementById('total').innerHTML = "$0,000.00";
-    }
-});
+var total=0;
 
-var checkbox2 = document.getElementById('checkbox2');
-checkbox2.addEventListener( 'change', function() {
-    if(this.checked) {
-        document.getElementById('total').innerHTML = "$4,499.00";
-    } else{
-        document.getElementById('total').innerHTML = "$0,000.00";
-    }
-});
+function sumar(valor) {
+total += valor;
+document.formulario.total.value=total;
+}
 
-
-var checkbox3 = document.getElementById('checkbox3');
-checkbox3.addEventListener( 'change', function() {
-    if(this.checked) {
-        document.getElementById('total').innerHTML = "$2,695.00";
-    } else{
-        document.getElementById('total').innerHTML = "$0,000.00";
-    }
-});
-
-
-var checkbox4 = document.getElementById('checkbox4');
-checkbox4.addEventListener( 'change', function() {
-    if(this.checked) {
-        document.getElementById('total').innerHTML = "$399.00";
-    } else{
-        document.getElementById('total').innerHTML = "$0,000.00";
-    }
-});
-
-
-var checkbox5 = document.getElementById('checkbox5');
-checkbox5.addEventListener( 'change', function() {
-    if(this.checked) {
-        document.getElementById('total').innerHTML = "$4,499.00";
-    } else{
-        document.getElementById('total').innerHTML = "$0,000.00";
-    }
-});
-
-
+function restar(valor) {
+total-=valor;
+document.formulario.total.value=total;
+}
