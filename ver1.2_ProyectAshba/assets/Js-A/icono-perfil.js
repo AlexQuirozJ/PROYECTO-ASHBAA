1dbc58d07befb69b0e32443babee39d8a9cc4b2e
ClaraItzel <81541685+ClaraItzel@users.noreply.github.com>
2021-08-03 23:45:05 -0500

    icono de foto

--- ver1.2_ProyectAshba/assets/Js-A/icono-perfil.js
+++ ver1.2_ProyectAshba/assets/Js-A/icono-perfil.js
@@ -5,3 +5,17 @@ icono.forEach(function(userItem) {
         userItem.classList.add('heart_animate');
     });
   });
+  let camara = document.querySelector('#camara')
+  camara.addEventListener('click',()=>{
+      alert('hola');
+      let foto = document.querySelector('#file');
+      foto.click();
+        foto.onchange=function(e){
+            let reader=new FileReader();
+            reader.readAsDataURL(e.target.files[0]);
+            reader.onload=function(){
+                let preview=document.getElementById("fotito");
+                preview.src=reader.result;
+            }
+        }
+  });
