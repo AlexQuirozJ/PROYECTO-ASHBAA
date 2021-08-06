83738bda57a96ee21fcc67f78566d034ec771e53
Sofia Lopez <anasolori.92@gmail.com>
2021-08-06 13:35:46 -0500

    Barra de búsqueda funcionando con API

--- ver1.2_ProyectAshba/assets/Js-A/respuestaFetch.js
+++ ver1.2_ProyectAshba/assets/Js-A/respuestaFetch.js
@@ -1,14 +1,17 @@
 const productosMostrados = document.getElementById('productosMostrados');
 const formularioBusqueda = document.querySelector('form');
+//Nos traemos la búsqueda del local storage
+let busqueda = localStorage.getItem('busqueda') || null;
 
+//Generamos un evento para iniciar llamar la función de búsqueda
 formularioBusqueda.addEventListener('submit', (e) => {
   e.preventDefault();
-  productosMostrados.innerHTML = '';
-  const search = document.getElementById('search');
+  const search = document.getElementById('label');
   let busqueda = search.value;
   cargarProductos(busqueda);
 });
 
+//Esta función llama la API y nos permite desplegar los productos que coincidadn con búsqueda
 const cargarProductos = async (busqueda) => {
   try {
     const res = await fetch(
@@ -22,6 +25,13 @@ const cargarProductos = async (busqueda) => {
   }
 };
 
+//si busqueda es true, entonces se activa la función de busqueda e inmediatamente se borra el elemento de local storage
+if (busqueda) {
+  cargarProductos(busqueda);
+  localStorage.removeItem('busqueda');
+}
+
+//Esta función es la que nos permite anexar los productos al html en el formato que se necesita
 const displayProductos = (products) => {
   const htmlString = products
     .map((product) => {
@@ -29,8 +39,8 @@ const displayProductos = (products) => {
       <div class="container producto">
           
               <div class="card mx-auto">
-            <a href="${product.link_pagina}">
-            <img src="${product.imagen}" id="imgAPI">
+          <a href="${product.link_pagina}" target="_blank">
+            <img src="${product.imagen}" class="imagen_bus" id="imgAPI">
           </a>
             <p style="font-family: Verdana, Geneva, Tahoma, sans-serif;">${product.name}</p>
           <h5 style="font-family: Verdana, Geneva, Tahoma, sans-serif;"><strong>${product.tienda}</strong></h5>
